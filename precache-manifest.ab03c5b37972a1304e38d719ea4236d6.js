1d5a8dd69214f2d219acab590c76705e1c10ef99
Sergey Ushakov <sUshakov@ntoire-polus.ru>
2018-11-29 17:13:11 +0300

    Updates

---
+++ precache-manifest.ab03c5b37972a1304e38d719ea4236d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f3f7adee9c86b455519",
+    "url": "/CompanyWebsite/static/js/runtime~main.0f3f7ade.js"
+  },
+  {
+    "revision": "4c0520496745f6e28d26",
+    "url": "/CompanyWebsite/static/js/main.4c052049.chunk.js"
+  },
+  {
+    "revision": "0ab79661c9fa36002a96",
+    "url": "/CompanyWebsite/static/js/1.0ab79661.chunk.js"
+  },
+  {
+    "revision": "4c0520496745f6e28d26",
+    "url": "/CompanyWebsite/static/css/main.9caf226b.chunk.css"
+  },
+  {
+    "revision": "15e563a728696b6ff38c08212533185c",
+    "url": "/CompanyWebsite/index.html"
+  }
+];
