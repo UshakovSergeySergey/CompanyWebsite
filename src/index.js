841729b1c85b07bb1c7bf8648d8f77fa22117908
Sergey Ushakov <sUshakov@ntoire-polus.ru>
2018-11-30 17:54:57 +0300

    trying to fix bootstrap dependencies

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+import $ from 'jquery';
+import Popper from 'popper.js';
 
 ReactDOM.render((
         <BrowserRouter>
