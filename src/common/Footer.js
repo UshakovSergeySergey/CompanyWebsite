b6bc6fbbf08efb4c66e1e4b6fc1760fd33b78b9b
Ushakov Sergey <ushakov.sergey.sergey@gmail.com>
2018-11-30 20:49:14 +0300

    trying to fix bootstrap

--- src/common/Footer.js
+++ src/common/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { WebSiteStructure } from '../WebSiteStructure';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 class Footer extends React.Component {
     getLinksToSections = () => {
--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
-import $ from 'jquery';
-import Popper from 'popper.js';
+//import 'bootstrap/dist/css/bootstrap.min.css';
+//import 'bootstrap/dist/js/bootstrap.bundle.min';
+//import $ from 'jquery';
+//import Popper from 'popper.js';
 
 ReactDOM.render((
         <BrowserRouter>
